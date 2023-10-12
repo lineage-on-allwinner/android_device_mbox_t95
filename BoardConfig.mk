2dcdfd86aec588f443d6c7606185ffc9b7b0ee16
bengris32 <bengris32@protonmail.ch>
2023-10-12 21:45:29 +0100

    t95: Set kernel DTB name
    Change-Id: I9f8d912e8832b73456f068051247a563164cfc2b
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,7 @@ DEVICE_PATH := device/mbox/t95
 
 # Kernel
 TARGET_KERNEL_CONFIG := t95_defconfig
+TARGET_KERNEL_DTB := sunxi/sun50iw9p1-t95-5g.dtb
 
 # Inherit from cupid-common
 include device/allwinner/cupid-common/BoardConfigCommon.mk
